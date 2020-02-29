e9c6303bd940f6421ab036f54f5aa1beb5c5b4f6
Carlos Gonzalez <carlos.rgn@gmail.com>
2020-02-28 18:34:20 -0600

    kafka-to-sql python startup

---
+++ apps/kafka-to-sql/start.py
@@ -0,0 +1,75 @@
+import os
+import sys
+import subprocess
+import re
+import time
+import signal
+import requests
+
+kafka_connect_host = "localhost:8083"
+
+os.environ["CONNECT_REST_ADVERTISED_HOST_NAME"] = (subprocess.check_output(
+    ["hostname", "-I"]).decode("UTF-8").split(" ")[0])
+
+subprocess.Popen(["/etc/confluent/docker/run"])
+
+wait_counter = 0
+print("Waiting for Kafka Connect API to be ready ⏳")
+while True:
+    try:
+        r = requests.get("http://{}/connectors".format(kafka_connect_host))
+        status = r.status_code
+        r.raise_for_status()
+    except (requests.ConnectionError):
+        status = 0
+    if status == 200:
+        break
+    else:
+        print("Kafka Connect API not ready yet... retry # {}".format(
+            wait_counter))
+        wait_counter += 1
+        if wait_counter < 15:
+            time.sleep(3)
+        else:
+            print("Waited too long!")
+            sys.exit(1)
+
+print("Creating Kafka Cdonnect Postgresql Sink")
+
+regex = re.compile(r"^postgresql://(?P<user>.+):(?P<pass>.+)@(?P<rest>.+)$")
+m = regex.match(os.environ["DB_URL"])
+
+r = requests.put(
+    "http://{}/connectors/jdbc_sink_postgresql_00/config".format(
+        kafka_connect_host),
+    json={
+        "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector",
+        "tasks.max": '1',
+        "topics": "users",
+        "connection.url": "jdbc:postgresql://{}".format(m.group("rest")),
+        "connection.user": m.group("user"),
+        "connection.password": m.group("pass"),
+        "auto.create": "false",
+        "insert.mode": "upsert",
+        "pk.mode": "record_value",
+        "pk.fields": "id",
+    },
+)
+print(r.status_code)
+r.raise_for_status()
+
+print("Done.")
+
+running = True
+
+
+def sigterm():
+    global running
+    running = False
+
+
+signal.signal(signal.SIGTERM, sigterm)
+
+while running:
+    time.sleep(2)
+print('Kafka Connect closing...')
